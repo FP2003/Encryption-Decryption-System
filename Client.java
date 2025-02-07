4620b615aa00e005434d97c373080939b6da5e10
JagvirH <jheer02@gmail.com>
2025-02-07 18:50:01 +0000

    Got data that needs to be sent to the server side

--- Client.java
+++ Client.java
@@ -18,9 +18,7 @@ public class Client {
         String serverHost = args[0];
         int serverPort = Integer.parseInt(args[1]);
         String userId = args[2];
-        
-        System.out.println("Connecting to server: " + serverHost + " on port: " + serverPort);
-        System.out.println("User ID: " + userId);
+
         
         try (Socket socket = new Socket(serverHost, serverPort);
              DataInputStream in = new DataInputStream(socket.getInputStream());
@@ -29,13 +27,17 @@ public class Client {
 
             // Send a basic authentication message (this will be replaced later)
             out.writeUTF("Hello, I'm a client with ID: " + userId);
-            
-            // Call firstServerCheck right after sending the initial message
+
             try {
-                firstServerCheck(userId);
+                // Call firstServerCheck
+                String[] result = firstServerCheck(userId);
+                System.out.println("Encrypted Data (Base64): " + result[0]);
+                System.out.println("Signature (Base64): " + result[1]);
             } catch (Exception e) {
+                System.err.println("Error occurred in firstServerCheck: " + e.getMessage());
                 e.printStackTrace();
             }
+            
 
             String serverResponse = in.readUTF();
             System.out.println(serverResponse);
@@ -64,28 +66,63 @@ public class Client {
         }
     }
 
-    public static void firstServerCheck(String userId) throws Exception {
-        System.out.println("Performing first server check for user: " + userId);
+    public static String[] firstServerCheck(String userId) throws Exception {
+        // Generate 16 fresh random bytes
+        byte[] randomBytes = new byte[16];
+        new SecureRandom().nextBytes(randomBytes);
 
-        // Need 3 components here
-        // Need the users name which is in the Variable userId
-        // need to generate 16 fresh random bytes
-        // Both of those then need to be encrypted with RSA/ECB/PKCS1Padding, this is i believe where we use Server.pub
-        // after is then signiture which i have 0 idea, but believe we need to use Alice.prv to encrypt it
+        // Convert random bytes to Base64
+        String randomBytesBase64 = Base64.getEncoder().encodeToString(randomBytes);
 
+        // Combine userId and random bytes into a readable string
+        String combinedData = userId + randomBytesBase64;
+        System.out.println("Combined Data: " + combinedData);
+
+        // Convert combined data to bytes
+        byte[] dataToEncrypt = combinedData.getBytes("UTF-8");
+
+        // Load Server's public key (Server.pub)
+        PublicKey serverPublicKey = loadPublicKey("Server.pub");
+
+        // Encrypt the combined userId + random bytes using RSA
         Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, serverPublicKey);
+        byte[] encryptedData = cipher.doFinal(dataToEncrypt);
 
-        //File f = new File(userId + ".prv");
-        File f = new File("Server.pub");
+        // Load User's private key (Alice.prv)
+        PrivateKey userPrivateKey = loadPrivateKey(userId + ".prv");
 
+        // Sign the encrypted data using SHA1withRSA
+        Signature signature = Signature.getInstance("SHA1withRSA");
+        signature.initSign(userPrivateKey);
+        signature.update(encryptedData);
+        byte[] signedData = signature.sign();
 
-        //byte[] keyBytes = Files.readAllBytes(f.toPath());
-        //PKCS8EncodedKeySpec prvSpec = new PKCS8EncodedKeySpec(keyBytes);
-        //KeyFactory kf = KeyFactory.getInstance("RSA");
-        //PrivateKey prvKey = kf.generatePrivate(prvSpec);
+        // Convert encrypted data and signature to Base64
+        String encryptedDataBase64 = Base64.getEncoder().encodeToString(encryptedData);
+        String signatureBase64 = Base64.getEncoder().encodeToString(signedData);
 
-        System.out.println("Performing first server check for user: " + userId);
+        // Return both encrypted data and signature
+        return new String[]{encryptedDataBase64, signatureBase64};
     }
+    
+
+    private static PublicKey loadPublicKey(String filename) throws Exception {
+        File keyFile = new File(filename);
+        byte[] keyBytes = Files.readAllBytes(keyFile.toPath());
+        X509EncodedKeySpec pubSpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePublic(pubSpec);
+    }
+
+    private static PrivateKey loadPrivateKey(String filename) throws Exception {
+        byte[] keyBytes = Files.readAllBytes(new File(filename).toPath());
+        PKCS8EncodedKeySpec prvSpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePrivate(prvSpec);
+    }
+
+    
 
     private static void receiveFile(String filename, DataInputStream in) throws IOException {
         int fileLength = in.readInt();
