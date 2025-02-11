df3b93fbe45a0982b8dc8a02dee23feb3b69a24b
FP2003 <filippieleckii@gmail.com>
2025-02-11 18:15:18 +0000

    Examples from blackboard

---
+++ ExamplesUsedInLabs/SignatureExample.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+import java.security.*;
+import java.nio.file.Files;
+
+public class SigExample {
+
+	public static void main(String [] args) throws Exception {
+
+		// Get private key to create the signature
+		File f = new File("alice.prv");
+		byte[] keyBytes = Files.readAllBytes(f.toPath());
+		PKCS8EncodedKeySpec prvSpec = new PKCS8EncodedKeySpec(keyBytes);
+		KeyFactory kf = KeyFactory.getInstance("RSA");
+		PrivateKey prvKey = kf.generatePrivate(prvSpec);
+
+		// create signature
+		Signature sig = Signature.getInstance("SHA256withRSA");
+		sig.initSign(prvKey);
+		sig.update("Hello world".getBytes());
+		byte[] signature = sig.sign();
+		
+		// prompt input
+		System.out.println("Enter the message to be verified:");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+
+		// read public key to verify signature
+		f = new File("alice.pub");
+		keyBytes = Files.readAllBytes(f.toPath());
+		X509EncodedKeySpec pubSpec = new X509EncodedKeySpec(keyBytes);
+		kf = KeyFactory.getInstance("RSA");
+		PublicKey pubKey = kf.generatePublic(pubSpec);
+
+		// verify signature
+		sig.initVerify(pubKey);
+		sig.update(input.getBytes());
+		boolean b = sig.verify(signature);
+		if (b) System.out.println("Signature verified");
+		else System.out.println("Signature not verified");
+
+	}
+}
